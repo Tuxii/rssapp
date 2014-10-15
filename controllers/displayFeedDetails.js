d2bf5da1d39a898a0ae64bfadfa6fb927e995aea
Pierre Deville <tuxihf@gmail.com>
2014-10-15 18:51:57 +0200

    Debut de bdd

--- controllers/displayFeedDetails.js
+++ controllers/displayFeedDetails.js
@@ -1,5 +1,7 @@
 var args = arguments[0] || {};
 
-alert(args.content);
-$.desc.text = args.text;
-Ti.API.info(JSON.stringify(args));
+var description = args.description || "";
+var content = args.content || "";
+
+
+$.feedDetails.html = args.description + args.content;
