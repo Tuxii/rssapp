8d0d6e878cf40785417de9c30ab96f81404d1451
Pierre Deville <tuxihf@gmail.com>
2014-10-16 20:10:45 +0200

    bdd

--- controllers/displayFeedDetails.js
+++ controllers/displayFeedDetails.js
@@ -3,5 +3,8 @@ var args = arguments[0] || {};
 var description = args.description || "";
 var content = args.content || "";
 
+function redirectToArticle(){
+	Ti.Platform.openURL(args.link);
+}
 
-$.feedDetails.html = args.description + args.content;
+$.feedDetails.html = description + content;
--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,7 @@
 var rss = require('rss');
+var db = require("db");
+db.loadDb();
+populate_feeds_list();
 
 function showDialog(){
      $.dialog.show();
@@ -7,15 +10,70 @@ function showDialog(){
 function doClick(e){
 	
 	
-		if(e.index == 0){ // clicked add feed
-			var url = $.addFeedInput.value;
+	if(e.index == 0){ // clicked add feed
+		var url = $.addFeedInput.value;
+		rss.parseFeed(url, function(data){
+			db.insert('feeds', {url: url, title: data.title});
+			populate_feeds_list();
+		});
+		
+	}
+
+};
+
+
+
+
+
+
+function populate_feeds_list(){
+	var feeds = db.find('feeds', "*");
+
+
+	var tableData = [];
+	
+	for(var i=0, j=feeds.length; i<j; i++){
+		var row = Ti.UI.createTableViewRow({
+			rowIndex: i,
+			height: 50,
+			selectedBackgroundColor: "white"
+		});
+		
+		var title = Ti.UI.createLabel({
+			text: feeds[i].title,
+			left:0
+		});
+		row.url = feeds[i].url;
+		var url = feeds[i].url;
+		row.add(title);
+		
+		tableData.push(row);
+		
+		row.addEventListener('click', function(e){
 			rss.parseFeed(url, function(data){
 				Alloy.createController('displayFeed', data).getView().open();
 			});
-			
-		}
+		});
+	}
+	$.feedsList.data = tableData;
+}
 
-};
+
+function displayLoading(){
+	
+	var activityIndicator = Ti.UI.createActivityIndicator({
+	  color: 'green',
+	  font: {fontFamily:'Helvetica Neue', fontSize:26, fontWeight:'bold'},
+	  message: 'Loading...',
+	  style:style,
+	  top:10,
+	  left:10,
+	  height:Ti.UI.SIZE,
+	  width:Ti.UI.SIZE
+	});
+
+	
+}
 
 
 $.index.open();
